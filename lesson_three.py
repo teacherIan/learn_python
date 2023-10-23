d272b4dc332ebc5474da37d58915a2283a04e26c
teacherIan <ian_malloy@ssis.asia>
2023-10-23 20:42:03 +0800

    updates

--- lesson_three.py
+++ lesson_three.py
@@ -1,12 +1,10 @@
 # Lesson input, lists and dictionaries
 
 # inputting data into Python is extremely easy
-name = input("Please input your name:") # note: all inputted data is a string datatype.
+name = input("Please input your name:")  # note: all inputted data is a string datatype.
 print("Your name is: " + name)
 num = int(input("Please enter a number"))
-print(num + 5) # this works correctly as I changed the datatype to int
-
-
+print(num + 5)  # this works correctly as I changed the datatype to int
 
 # dictionaries store data in key / value pairs
 
@@ -15,19 +13,19 @@ first_dictionary = {
     "age": 40,
     "job": "teacher"
 }
-
+# use the get method to return the value from a key
 print(first_dictionary.get("name"))  # prints 'Ian'
 print(first_dictionary.get("age"))  # prints 40
 
 # dictionaries have a few ways of being updated
 first_dictionary.update([('location', 'China')])
 print(first_dictionary)
+first_dictionary['home'] = "USA"
+print(first_dictionary)
 
-my_input = input("Please enter something:")
+# lists store indexed data
+my_list = [1, "2", 3.0, first_dictionary]  # lists can store different types of data
 
-print("your input was: " + my_input)
+#
 
-hi = "Hello"
-hi = "bye"
 
-print(hi)
